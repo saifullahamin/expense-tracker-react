9662b97db87b6d214467180ca252c5745b63a473
Saifullah Amin <saifullahaminkhan@gmail.com>
2020-11-30 16:35:13 +0500

    Enhanced the delete function.

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -13,18 +13,22 @@ export const TransactionList = () => {
       <ul className="transaction-list">
         {transactions.map((transObj) => {
           return (
-            <li className={transObj.amount < 0 ? "minus" : "plus"} key={transObj.id}>
-              <span>{transObj.desc}</span>
+            <li
+              className={transObj.amount < 0 ? "minus" : "plus"}
+              key={transObj.id}
+            >
+              <div>
               <button
-                className="x-button"
-                onClick={() => deleteTransaction(transObj.id)}
-              >
-                x
-              </button>
+                  className="x-button"
+                  onClick={() => deleteTransaction(transObj.id)}
+                >
+                  x
+                </button>
+                <span>{transObj.desc}</span>
+              </div>
               <span>
                 {transObj.amount >= 0 ? "+" : "-"}${Math.abs(transObj.amount)}
               </span>
-             
             </li>
           );
         })}
@@ -32,4 +36,4 @@ export const TransactionList = () => {
       <br />
     </div>
   );
-}
+};
