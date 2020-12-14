7d51b3db3a104d9d4f1997e3498e030002958c4e
Saifullah Amin <saifullahaminkhan@gmail.com>
2020-12-15 00:22:16 +0500

    Enhanced the code for understandibilty

--- src/TransactionContext.js
+++ src/TransactionContext.js
@@ -1,29 +1,3 @@
-import { createContext, useReducer } from "react";
-import TransactionReducer from "./TransactionReducer";
+import { createContext } from "react";
 
-let initialTransactions = [];
-
-export const TransactionContext = createContext(initialTransactions);
-
-export const TransactionProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(TransactionReducer, initialTransactions);
-  function addTransaction(transObj) {
-    dispatch({
-      type: "ADD",
-      payload: transObj,
-    });
-  }
-  function deleteTransaction(id) {
-    dispatch({
-      type: "DELETE",
-      payload: id,
-    });
-  }
-  return (
-    <TransactionContext.Provider
-      value={{ transactions: state, addTransaction, deleteTransaction }}
-    >
-      {children}
-    </TransactionContext.Provider>
-  );
-};
+export const TransactionContext = createContext([]);
